d8f4b472297a83a1097635102cfbeab7ff5fd72e
kairi003 <kairi.satellite@gmail.com>
2026-04-03 03:23:46 +0900

    change: lux version that supports both comments and chat

--- content.js
+++ content.js
@@ -1,50 +1,43 @@
 //@ts-check
 
-const inject = () => {
-  const script = document.createElement('script');
-  script.src = chrome.runtime.getURL('inject.js');
-  document.documentElement.appendChild(script);
+/** @param {HTMLImageElement} img */
+const getText = (img) => {
+  const { alt, dataset: { emojiId } } = img;
+  // Not youtube emoji, just return alt text
+  if (!emojiId) return alt || '';
+  // For youtube official emoji, return :alt:
+  if (emojiId.startsWith('UCkszU2WH9gy1mb0dV-11UJg/')) return `:${alt}:`;
+  // For custom emoji, return :_alt:
+  return `:_${alt}:`;
 }
 
-/** @param {HTMLElement} input */
-const register = (input) => {
-  input.addEventListener('paste', (event) => {
-    const data = event.clipboardData?.getData('text/html');
-    const content = data?.match(/<!--StartFragment-->([\s\S]*?)<!--EndFragment-->/)?.[1];
-    /** @type {{ emojiId: string, shortcuts: string[] }[]} */
-    const emojis = JSON.parse(document.body.dataset.emojis || '[]');
-    if (!content || !emojis) return;
+/** @param {ClipboardEvent} event */
+const halder = (event) => {
+  const data = event.clipboardData?.getData('text/html');
+  const content = data?.match(/<!--StartFragment-->([\s\S]*?)<!--EndFragment-->/)?.[1];
+  if (!content) return;
 
-    event.preventDefault();
-    event.stopPropagation();
+  event.preventDefault();
+  event.stopPropagation();
 
-    const div = document.createElement('div');
-    div.innerHTML = content;
-    console.debug('Parsed content:', div);
-    for (const ch of div.childNodes) {
-      console.debug('Processing child node:', ch);
-      if (ch instanceof HTMLImageElement) {
-        const emoji = emojis.find(e => e.emojiId === ch.dataset.emojiId);
-        const text = emoji?.shortcuts?.[0] || ch.alt || '';
-        document.execCommand('insertHTML', false, text);
-      } else {
-        document.execCommand('insertHTML', false, ch.textContent || '');
-      }
+  const div = document.createElement('div');
+  div.innerHTML = content;
+  for (const ch of div.childNodes) {
+    if (ch instanceof HTMLImageElement) {
+      document.execCommand('insertHTML', false, getText(ch));
+    } else {
+      document.execCommand('insertHTML', false, ch.textContent || '');
     }
-  }, { capture: true });
+  }
 }
 
 const check = () => {
-  const input = document.querySelector('div#input[contenteditable]');
+  const input = document.querySelector('div#input[contenteditable],div#contenteditable-root[contenteditable]');
   if (input instanceof HTMLElement) {
-    register(input);
-    return true;
+    input.removeEventListener('paste', halder, { capture: true });
+    input.addEventListener('paste', halder, { capture: true });
   }
-  return false;
 }
 
-check() || new MutationObserver((_, observer) => {
-  if (check()) observer.disconnect();
-}).observe(document, { childList: true, subtree: true });
-
-inject();
+new MutationObserver(check).observe(document, { childList: true, subtree: true });
+check()
--- inject.js
+++
@@ -1,2 +0,0 @@
-document.body.dataset.emojis = JSON.stringify(window.ytInitialData.continuationContents.liveChatContinuation.emojis);
-console.log("Extracted emojis from ytInitialData:", document.body.dataset.emojis);
